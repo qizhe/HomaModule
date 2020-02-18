7ce4ad374bf701379648b8254899991a9185d620
qizhe <qizhe.cai@gmail.com>
2020-02-18 16:20:17 -0500

    add offload

--- test/unit_homa_offload.c
+++ test/unit_homa_offload.c
@@ -25,7 +25,7 @@ extern struct homa *homa;
 FIXTURE(homa_offload) {
 	__be32 ip;
 	struct data_header header;
-	struct sk_buff *gro_list;
+	struct list_head gro_list;
 };
 FIXTURE_SETUP(homa_offload)
 {
@@ -51,18 +51,32 @@ FIXTURE_SETUP(homa_offload)
 	NAPI_GRO_CB(skb2)->same_flow = 1;
 	NAPI_GRO_CB(skb2)->data_offset = sizeof32(struct data_header);
 	NAPI_GRO_CB(skb2)->last = skb2;
-	self->gro_list = skb;
-	skb->next = skb2;
-	skb2->next = NULL;
+	INIT_LIST_HEAD(&self->gro_list);
+	list_add_tail(&skb->list, &self->gro_list);
+	list_add_tail(&skb2->list, &self->gro_list);
+	// self->gro_list = skb;
+	// skb->next = skb2;
+	// skb2->next = NULL;
 	unit_log_clear();
 }
 FIXTURE_TEARDOWN(homa_offload)
 {
-	while (self->gro_list) {
-		struct sk_buff *next = self->gro_list->next;
-		kfree_skb(self->gro_list);
-		self->gro_list = next;
+	struct sk_buff *p, *p_prev = NULL;
+	list_for_each_entry(p, &self->gro_list, list) {
+		if(p_prev != NULL) {
+			kfree_skb(p_prev);
+			p_prev = NULL;
+		}
+		p_prev = p;
 	}
+	if(p_prev != NULL) {
+		kfree_skb(p_prev);
+	}
+	// while (self->gro_list) {
+	// 	struct sk_buff *next = self->gro_list->next;
+	// 	kfree_skb(self->gro_list);
+	// 	self->gro_list = next;
+	// }
 	unit_teardown();
 }
 
@@ -81,7 +95,7 @@ TEST_F(homa_offload, homa_gro_receive__header_too_short)
 }
 TEST_F(homa_offload, homa_gro_receive__append)
 {
-	struct sk_buff *skb, *skb2;
+	struct sk_buff *skb, *skb2, *next;
 	int same_flow;
 	self->header.seg.offset = htonl(6000);
 	skb = mock_skb_new(self->ip, &self->header.common, 1400, 0);
@@ -93,7 +107,14 @@ TEST_F(homa_offload, homa_gro_receive__append)
 	NAPI_GRO_CB(skb)->same_flow = 1;
 	homa_gro_receive(&self->gro_list, skb2);
 	
-	unit_log_frag_list(self->gro_list->next, 1);
+
+
+	next = list_first_entry(&self->gro_list, typeof(*next), list);
+	next = list_next_entry(next, list);
+
+	unit_log_frag_list(next, 1);
+
+	// unit_log_frag_list(self->gro_list->next, 1);
 	EXPECT_STREQ("DATA from 196.168.0.1:40000, dport 88, id 1001, "
 			"prio 0, message_length 10000, offset 6000, "
 			"data_length 1400, incoming 10000, cutoff_version 0; "
@@ -108,7 +129,7 @@ TEST_F(homa_offload, homa_gro_receive__append)
 }
 TEST_F(homa_offload, homa_gro_receive__max_gro_skbs)
 {
-	struct sk_buff *skb, *skb2, *skb3;
+	struct sk_buff *skb, *skb2, *skb3, *next;
 	int same_flow;
 	
 	homa->max_gro_skbs = 3;
@@ -129,7 +150,11 @@ TEST_F(homa_offload, homa_gro_receive__max_gro_skbs)
 	NAPI_GRO_CB(skb3)->same_flow = 0;
 	EXPECT_NE(NULL, homa_gro_receive(&self->gro_list, skb3));
 	
-	unit_log_frag_list(self->gro_list->next, 1);
+	next = list_first_entry(&self->gro_list, typeof(*next), list);
+	next = list_next_entry(next, list);
+	unit_log_frag_list(next, 1);
+	
+	// unit_log_frag_list(self->gro_list->next, 1);
 	EXPECT_STREQ("DATA from 196.168.0.1:40000, dport 88, id 1001, "
 			"prio 0, message_length 10000, offset 6000, "
 			"data_length 1400, incoming 10000, cutoff_version 0; "
